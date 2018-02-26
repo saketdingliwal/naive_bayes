eead16c4031df30975f4f9a683ff12cbd9326d67
LogicalFreaks <saketdingliwal97@gmail.com>
2018-02-27 03:10:43 +0530

    confusion matrix

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,5 +1,29 @@
 import numpy as np
 import math
+import random
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+import sys
+
+#initializing stemmer
+tokenizer = RegexpTokenizer(r'\w+')
+en_stop = set(stopwords.words('english'))
+p_stemmer = PorterStemmer()
+
+# function that takes an input file and performs stemming to generate the output file
+def getStemmedDocument(input_raw_data):
+	docs = input_raw_data
+	for doc in docs:
+		raw = doc.lower()
+		raw = raw.replace("<br /><br />", " ")
+		tokens = tokenizer.tokenize(raw)
+		stopped_tokens = [token for token in tokens if token not in en_stop]
+		stemmed_tokens = [p_stemmer.stem(token) for token in stopped_tokens]
+		documentWords = ' '.join(stemmed_tokens)
+	return documentWords
+
+
 def read_data(file_name):
 	file = open("data/" + file_name,"r")
 	all_text = file.readlines()
@@ -7,13 +31,10 @@ def read_data(file_name):
 		print "empty document"
 	return all_text
 
-
-
-
 training_data = read_data("train_sample.txt")
 training_labels = read_data("train_sample_labels.txt")
-
-
+test_data = read_data("test_sample_text.txt")
+test_labels = read_data("test_sample_labels.txt")
 
 def make_dict(training_data):
 	count = 0
@@ -27,7 +48,7 @@ def make_dict(training_data):
 	return vocab_dict
 
 
-label_dict = make_dict(training_labels)
+label_dict = make_dict(training_labels)	
 number_classes = len(label_dict)
 vocab_dict = make_dict(training_data)
 
@@ -67,5 +88,48 @@ def predict(test_document):
 			predicted_class = class_
 	return predicted_class
 
-ans = predict(training_data[0])
-print ans
+
+def accuracy(test_documents,labels):
+	correct = 0.0
+	for i in range(len(test_documents)):
+		predicted_class = predict(test_documents[i])
+		if labels[i].split()[0] in label_dict.keys():
+			expected_class = label_dict[labels[i].split()[0]]
+			if predicted_class==expected_class:
+				correct+=1
+	return correct/len(test_documents)
+
+def majority_accuracy(test_documents,labels):
+	correct = 0.0
+	for i in range(len(test_documents)):
+		predicted_class = np.argmax(label_freq)
+		if labels[i].split()[0] in label_dict.keys():
+			expected_class = label_dict[labels[i].split()[0]]
+			if predicted_class==expected_class:
+				correct+=1
+	return correct/len(test_documents)
+
+def random_accuracy(test_documents,labels):
+	correct = 0.0
+	for i in range(len(test_documents)):
+		predicted_class = random.randint(0,len(label_dict))
+		if labels[i].split()[0] in label_dict.keys():
+			expected_class = label_dict[labels[i].split()[0]]
+			if predicted_class==expected_class:
+				correct+=1
+	return correct/len(test_documents)
+
+def confusion_matrix(test_documents,labels):
+	correct = np.zeros((len(label_dict),len(label_dict)))
+	for i in range(len(test_documents)):
+		predicted_class = predict(test_documents[i])
+		if labels[i].split()[0] in label_dict.keys():
+			expected_class = label_dict[labels[i].split()[0]]
+			correct[expected_class][predicted_class] += 1
+	return correct
+
+
+training_accuracy = accuracy(training_data,training_labels)
+test_accuracy = confusion_matrix(test_data,test_labels)
+# print test_accuracy
+print test_accuracy
